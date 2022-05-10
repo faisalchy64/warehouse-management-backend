5b103f57db4c7a309276f8a63b879774a08555fb
Faisal Karim <faisalchy64@gmail.com>
2022-05-10 20:12:38 +0600

    get data from database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -9,6 +11,33 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@warehouse-management.uza0x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const collection = client.db("fruitbase").collection("fruits");
+
+        // get data from database
+        app.get("/items", async (req, res) => {
+            const query = {};
+            const cursor = collection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
