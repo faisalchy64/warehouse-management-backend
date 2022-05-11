eacefed1e833d5ffa28ef4ccc02dab68081852a4
Faisal Karim <faisalchy64@gmail.com>
2022-05-11 22:32:29 +0600

    delete a specific data from server

--- index.js
+++ index.js
@@ -62,6 +62,17 @@ async function run() {
 
             res.send(result);
         });
+
+        // delete a specific data from server
+
+        app.delete("/item/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+
+            const result = await collection.deleteOne(query);
+
+            res.send(result);
+        });
     } finally {
         // await client.close();
     }
